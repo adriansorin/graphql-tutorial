68297586633912adf89e9130d9ba5bd08945d65c
Adrian Avram <avram.adrian.sorin@hotmail.co.uk>
2019-01-09 01:11:22 +0200

    add mocking

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
 import { ApolloProvider, Query } from 'react-apollo';
+import { SchemaLink } from 'apollo-link-schema';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
 import { typeDefs } from './schema';
 
 const schema = makeExecutableSchema({ typeDefs });
 addMockFunctionsToSchema({ schema });
-const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+console.log(new SchemaLink({ schema }));
 
 const client = new ApolloClient({
-  networkInterface: mockNetworkInterface
+  link: new SchemaLink({ schema }),
+  cache: new InMemoryCache(window.__APOLLO_STATE__)
 });
 
 const ChannelsList = () => (
