906a512325cf46691f0af355897c4c6928f92b17
Adrian Avram <avram.adrian.sorin@hotmail.co.uk>
2019-01-09 23:25:22 +0200

    add server part

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,10 @@ import './App.css';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
 import { ApolloProvider, Query } from 'react-apollo';
-import { SchemaLink } from 'apollo-link-schema';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import { typeDefs } from './schema';
-
-const schema = makeExecutableSchema({ typeDefs });
-addMockFunctionsToSchema({ schema });
-console.log(new SchemaLink({ schema }));
 
 const client = new ApolloClient({
-  link: new SchemaLink({ schema }),
+  uri: 'http://localhost:4001/graphql',
   cache: new InMemoryCache(window.__APOLLO_STATE__)
 });
 
